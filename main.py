73571d98275074b6bd75b4757111a2484bdc9d75
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-09 18:04:27 +0530

    Completed

--- main.py
+++ main.py
@@ -6,8 +6,26 @@ import threading
 import imutils
 import time
 
+stream = cv2.VideoCapture("clip.mp4")
+flag = True
 def play(speed):
-    print(f"wtf you clicked speed is {speed}")
+    global flag
+    print(f"You clicked on play. Speed is {speed}")
+
+    # Play the video in reverse mode
+    frame1 = stream.get(cv2.CAP_PROP_POS_FRAMES)
+    stream.set(cv2.CAP_PROP_POS_FRAMES, frame1 + speed)
+
+    grabbed, frame = stream.read()
+    if not grabbed:
+        exit()
+    frame = imutils.resize(frame, width=SET_WIDTH, height=SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image=frame, anchor=tkinter.NW)
+    if flag:
+        canvas.create_text(134, 26, fill="black", font="Times 26 bold", text="Decision Pending")
+    flag = not flag
 
 def pending(decision):
     frame = cv2.cvtColor(cv2.imread("pending.png"), cv2.COLOR_BGR2RGB)
@@ -62,9 +80,9 @@ btn = tkinter.Button(window,text="<< Previous (fast)",width=50,command=partial(p
 btn.pack()
 btn = tkinter.Button(window,text="<< Previous (slow)",width=50,command=partial(play,-2))
 btn.pack()
-btn = tkinter.Button(window,text="Next (fast) >>",width=50,command=partial(play,2))
+btn = tkinter.Button(window,text="Next (fast) >>",width=50,command=partial(play,25))
 btn.pack()
-btn = tkinter.Button(window,text="Next (slow) >>",width=50,command=partial(play,25))
+btn = tkinter.Button(window,text="Next (slow) >>",width=50,command=partial(play,2))
 btn.pack()
 btn = tkinter.Button(window,text="Give Out",width=50,command=out)
 btn.pack()
