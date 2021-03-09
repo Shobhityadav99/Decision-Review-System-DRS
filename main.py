b7005ad6c6e8fb73ae8f4a8d6cf6b94a2d15354d
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-09 14:53:35 +0530

    Welcome page displayed

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ window.title("Third Umpire DRS")
 cv_img = cv2.cvtColor(cv2.imread("welcome.png"), cv2.COLOR_BGR2RGB)
 canvas = tkinter.Canvas(window,width=SET_WIDTH,height=SET_HEIGHT)
 photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
+image_on_canvas =canvas.create_image(0,0,ancho = tkinter.NW,image = photo)
+canvas.pack()
 
 
 window.mainloop()
