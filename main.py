ec9f92391eb1f096e55354f8fe5e4c5857eb1d3b
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-09 14:51:37 +0530

    Image brought

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ SET_HEIGHT=368
 
 window = tkinter.Tk()
 window.title("Third Umpire DRS")
+cv_img = cv2.cvtColor(cv2.imread("welcome.png"), cv2.COLOR_BGR2RGB)
 canvas = tkinter.Canvas(window,width=SET_WIDTH,height=SET_HEIGHT)
+photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
 
 
 window.mainloop()
