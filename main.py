f9bbe6258fe07ff4f748995402c4ab8ca59b649f
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-09 14:21:03 +0530

    Base window set

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter
+import cv2
+import PIL.Image , PIL.ImageTk
+
+SET_WIDTH=650
+SET_HEIGHT=368
+
+window = tkinter.Tk()
+window.title("Third Umpire DRS")
+canvas = tkinter.Canvas(window,width=SET_WIDTH,height=SET_HEIGHT)
+
+
+window.mainloop()
